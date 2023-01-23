bc0b0bb42e0c02c722c80da8ecb4a3daedbd4c58
jahir <rana78085@gmail.com>
2023-01-23 07:42:50 +0600

    Add some comment on js

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
+// Get html element
 const container = document.querySelector(".container");
 const refreshBtn = document.querySelector(".refresh-palette");
 
+// Create how many color palette have
 const maxPaletteBoxes = 32;
 
+// Create a function for create color palette
 const colorGeneratePalette = () => {
-
   container.innerHTML = "";
 
+  // for loop of color palette
   for (let i = 0; i < maxPaletteBoxes; i++) {
     let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
     randomHex = `#${randomHex.padStart(6, "0")}`;
 
+    // create li html element
     const color = document.createElement("li");
-    console.log(color);
     color.classList.add("color");
     color.innerHTML = `<div class="rect-box" style="background: ${randomHex}"></div>
                         <span class="hex-value">${randomHex}</span>`;
     container.appendChild(color);
 
+    // click on color palette for copied hex code
     color.addEventListener("click", () => copyColor(color, randomHex));
   }
 };
@@ -28,8 +32,8 @@ const copyColor = (element, hexValue) => {
   const colorElement = element.querySelector(".hex-value");
   navigator.clipboard.writeText(hexValue).then(() => {
     colorElement.innerText = "COPIED";
-    setInterval(() => colorElement.innerText = hexValue, 1000);
+    setInterval(() => (colorElement.innerText = hexValue), 1000);
   });
-}
+};
 
 refreshBtn.addEventListener("click", colorGeneratePalette);
