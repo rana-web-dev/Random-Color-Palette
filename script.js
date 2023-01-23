df4414771ded7290ba755b54e1300d6e9717920a
jahir <rana78085@gmail.com>
2023-01-23 08:01:52 +0600

    Add comment on js

--- script.js
+++ script.js
@@ -9,29 +9,35 @@ const maxPaletteBoxes = 32;
 const colorGeneratePalette = () => {
   container.innerHTML = "";
 
-  // for loop of color palette
+  // For loop of color palette
   for (let i = 0; i < maxPaletteBoxes; i++) {
+    // Randomly generate color code by Math.floor() function 
     let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
     randomHex = `#${randomHex.padStart(6, "0")}`;
 
-    // create li html element
+    // Create li html element
     const color = document.createElement("li");
     color.classList.add("color");
     color.innerHTML = `<div class="rect-box" style="background: ${randomHex}"></div>
                         <span class="hex-value">${randomHex}</span>`;
     container.appendChild(color);
 
-    // click on color palette for copied hex code
+    // Click on color palette for copied hex code
     color.addEventListener("click", () => copyColor(color, randomHex));
   }
 };
 
+// Call color generate palette. If you don't call this function the color palette will be increment. But you need to change color in the same place.
 colorGeneratePalette();
 
 const copyColor = (element, hexValue) => {
   const colorElement = element.querySelector(".hex-value");
+
+  // Use navigator.clipboard.writeText for copy hex code.
   navigator.clipboard.writeText(hexValue).then(() => {
     colorElement.innerText = "COPIED";
+
+    // After copied the copied text will be change to hex code in 1 second.
     setInterval(() => (colorElement.innerText = hexValue), 1000);
   });
 };
